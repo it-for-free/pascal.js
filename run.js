7c54b12199f8709844e132f04d866fa7bcbd248d
vedro-compota <ru-ibn-hottab@yandex.ru>
2021-05-15 18:51:09 +0300

    Добавлена папка src/.

--- run.js
+++ run.js
@@ -1,8 +1,8 @@
-const FileIO = require('./IO/FileIO.js');
-const ConsoleOutput = require('./IO/ConsoleOutput.js');
-const LexicalAnalyzer = require('./LexicalAnalyzer/LexicalAnalyzer.js');
-const SyntaxAnalyzer = require('./SyntaxAnalyzer/SyntaxAnalyzer.js');
-const Engine = require('./Semantics/Engine.js');
+const FileIO = require('./src/IO/FileIO.js');
+const ConsoleOutput = require('./src/IO/ConsoleOutput.js');
+const LexicalAnalyzer = require('./src/LexicalAnalyzer/LexicalAnalyzer.js');
+const SyntaxAnalyzer = require('./src/SyntaxAnalyzer/SyntaxAnalyzer.js');
+const Engine = require('./src/Semantics/Engine.js');
 
 
 var fileIO = new FileIO('example.pas', new ConsoleOutput());
