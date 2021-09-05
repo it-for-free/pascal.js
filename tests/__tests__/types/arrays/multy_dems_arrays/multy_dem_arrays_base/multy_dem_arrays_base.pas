8a8862ccbe54ca6367c2ecc6f5c3f6063bdad0d0
vedro-compota <ru-ibn-hottab@yandex.ru>
2021-09-05 17:46:00 +0300

    out of range problem

--- tests/__tests__/types/arrays/multy_dems_arrays/multy_dem_arrays_base/multy_dem_arrays_base.pas
+++ tests/__tests__/types/arrays/multy_dems_arrays/multy_dem_arrays_base/multy_dem_arrays_base.pas
@@ -1,10 +1,14 @@
 var
    a : array [1..10, 1..3] of integer;
-   a3 : integer;
+   b : array [2..5] of array [1..2] of integer;
+   a3, b3 : integer;
 begin  
    a[1][1] := 3;
    a[2][1] := 1;
    a[3][1] := a[1][1] + a[2][1] + 1;
 
+   b[3, 1] := a[3][1];
+
    a3 := a[3][1];
+   b3 := b[3][1]
 end.
